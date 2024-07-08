4e57955a1d90d9ad48f87491a359d96db8807bfe
Baris <baris1.basol@gmail.com>
2024-07-08 14:03:28 +0300

    Update mech_weapon.cs

--- Assets/Scripts/mech scripts/mech_weapon.cs
+++ Assets/Scripts/mech scripts/mech_weapon.cs
@@ -11,8 +11,15 @@ public class mech_weapon : MonoBehaviour
     private Player_new player_mouse_input;
     public  StarterAssets.StarterAssetsInputs player_sai;
     
-    float mouse_input_x;
-    float mouse_input_y;
+    float _mouse_input_x;
+    float _mouse_input_y;
+
+    float _x_axis;
+    float _x_axis_angle;
+    float _y_axis;
+    public float sensitivity;
+
+    public GameObject rotation_object;
 
     public GameObject mech_cannon;
 
@@ -32,24 +39,40 @@ public class mech_weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        handleInputs();  
+        handleInputs();
+        //go_cannon();
+        //rotate_cannon();
+        rotate_cannon_new();
     }
 
     void handleInputs() 
     {
-        mouse_input_x = player_mouse_input.player.player_mouse_x.ReadValue<float>();
-        mouse_input_y = player_mouse_input.player.player_mouse_y.ReadValue<float>();
+        _mouse_input_x = player_mouse_input.player.player_mouse_x.ReadValue<float>();
+        _mouse_input_y = player_mouse_input.player.player_mouse_y.ReadValue<float>();
         //tmp_x.text = mouse_input_x.ToString();
         //tmp_y.text = mouse_input_y.ToString();
         tmp_x.text = player_sai.look.x.ToString();
         tmp_y.text = player_sai.look.y.ToString();
+        _x_axis += player_sai.look.x * Time.deltaTime * sensitivity;
+        _y_axis += player_sai.look.y * Time.deltaTime * sensitivity;
+
+    }
+
+    void go_cannon()
+    {
+        mech_cannon.transform.position = gameObject.transform.position * Time.deltaTime;
+    }
+    void rotate_cannon() 
+    {
+        _x_axis_angle -= _y_axis;
+        _x_axis_angle = Mathf.Clamp(_x_axis_angle, -90, 90);
+        mech_cannon.transform.localRotation = Quaternion.Euler(_x_axis,0f,0f);
+        mech_cannon.transform.Rotate(Vector3.up * _x_axis);
     }
 
 
-    void rotate_cannon() 
+    void rotate_cannon_new() 
     {
-    //    Quaternion mech_new_rotation;
-    //    mech_new_rotation = 
-    //        //(player_sai.look.x,0f,player_sai.look.y);
+        mech_cannon.transform.rotation = rotation_object.transform.rotation;
     }
 }
